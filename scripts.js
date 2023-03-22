2363d30f07e8a022e9de6e2ae9b2404f1abd9e04
Jackie Lim <jervie.lim@gmail.com>
2023-03-23 00:46:54 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,79 @@
+// scripts.js
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+const temperatureEl = document.getElementById("temperature");
+const locationEl = document.getElementById("location");
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const backgroundImg = document.querySelector(".background img");
+
+function updateTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const date = now.toLocaleDateString();
+
+  timeEl.textContent = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+  dateEl.textContent = date;
+}
+
+function updateWeather() {
+  // Add your API key and fetch the weather data
+  const apiKey = "46890e5e9527949e6b3bf156ff3e693a";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      temperatureEl.textContent = `${Math.round(data.main.temp)}°C`;
+      locationEl.textContent = data.name;
+    });
+}
+
+function updateQuote() {
+  // Fetch a random quote
+  const apiUrl = "https://api.quotable.io/random";
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      quoteEl.textContent = data.content;
+      authorEl.textContent = `- ${data.author}`;
+    });
+}
+
+function updateBackground() {
+  const unsplashApiKey = "8be1194f4124330d393a45134d07f1091502e64817bc316ecf4b49b8daea5262";
+  const apiUrl = `https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=${unsplashApiKey}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      backgroundImg.src = data.urls.regular;
+    });
+}
+
+function addTask() {
+  const task = document.createElement("li");
+  task.textContent = taskInput.value;
+  taskList.appendChild(task);
+  taskInput.value = "";
+}
+
+function init() {
+  updateTime();
+  updateWeather();
+  updateQuote();
+  updateBackground();
+  setInterval(updateTime, 1000);
+
+  taskInput.addEventListener("keydown", event => {
+    if (event.key === "Enter" && taskInput.value.trim() !== "") {
+      addTask();
+    }
+  });
+}
+
+init();
